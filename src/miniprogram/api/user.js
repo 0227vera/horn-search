39d19a6cf243357979bb5246477179dbc95094e7
salvatoreliaoxuan <salvatoreliaoxuan@didiglobal.com>
2023-08-21 18:02:11 +0800

    perf: code

--- src/miniprogram/api/user.js
+++ src/miniprogram/api/user.js
@@ -6,4 +6,6 @@ export const addUser = params => request({ route: 'user/create', params })
 
 export const deleteUser = params => request({ route: 'user/delete', params })
 
+export const updateUserInfo = params => request({ route: 'user/update', params })
+
 export const getCurrentRole = params => request({ route: 'user/currentcole', params })
