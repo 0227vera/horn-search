a3921e0d59f6d5fa66e4237651bbd55d29a494ad
salvatoreliaoxuan <salvatoreliaoxuan@didiglobal.com>
2023-08-24 18:00:22 +0800

    feat: 订阅消息

--- src/functions/cloud/config/msg_config.js
+++ src/functions/cloud/config/msg_config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  NEW_ORDER_NOTICE: 'FwPgLeCwk4f0dP5_nxd9b6V_616TlhG1RdlWGcR_czw' // 新订单提醒
+  NEW_ORDER_NOTICE: 'FwPgLeCwk4f0dP5_nxd9b6V_616TlhG1RdlWGcR_czw', // 新订单提醒 to worker
+  ORDER_UPDATE: 'Z6UbxDMOx4Mfe2waRr_1vJ7mTsfn2gEWsSfrs27J1xQ' // 订单状态变更 to boss
 }
--- src/functions/cloud/framework/utils/notice_util.js
+++ src/functions/cloud/framework/utils/notice_util.js
@@ -1,39 +1,40 @@
 const UserModel = require('../../project/model/user_model.js')
 const ReleaseModel = require('../../project/model/release_model.js')
-const { sendMiniOnceTempMsg, customerServiceMessage } = require('../lib/mini_lib.js')
+const { sendMiniOnceTempMsg } = require('../lib/mini_lib.js')
 const { NEW_ORDER_NOTICE } = require('../../config/msg_config.js')
 
 const sendToWorkers = async function(id) {
   const res = await UserModel.getList({})
-  console.log('userList------->', res)
   const order = await ReleaseModel.getOne({ _id: id })
-  console.log('order------->', order)
   for (let i = 0; i < res.list.length; i++) {
     let item = res.list[i]
-    sendMiniOnceTempMsg({
-      touser: item.OPENID,
-      page: `/pages/page?order_id=${id}`,
-      lang: 'zh_CN',
-      templateId: NEW_ORDER_NOTICE,
-      miniprogramState: 'trial',
-      data: {
-        thing1: {
-          value: order.categoryName
-        },
-        thing2: {
-          value: order.categoryName
-        },
-        amount4: {
-          value: +order.price
-        },
-        character_string3: {
-          value: order._id
-        },
-        thing8: {
-          value: order.note
+    if (item.role === 'worker') {
+      const path = encodeURIComponent(`/order-detail/pages/detail-to-worker?id=${id}`)
+      sendMiniOnceTempMsg({
+        touser: item.OPENID,
+        page: `/pages/page?type=mp&path=${path}`,
+        lang: 'zh_CN',
+        templateId: NEW_ORDER_NOTICE,
+        miniprogramState: 'trial',
+        data: {
+          thing1: {
+            value: order.categoryName
+          },
+          thing2: {
+            value: order.categoryName
+          },
+          amount4: {
+            value: +order.price
+          },
+          character_string3: {
+            value: order._id
+          },
+          thing8: {
+            value: order.note
+          }
         }
-      }
-    })
+      })
+    }
   }
 }
 
