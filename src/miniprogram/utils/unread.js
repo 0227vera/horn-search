e51adedd33d61f94a8677ad8b33c537c47b98601
salvatoreliaoxuan <salvatoreliaoxuan@didiglobal.com>
2023-09-13 15:48:51 +0800

    perf: worker

--- src/miniprogram/utils/unread.js
+++ src/miniprogram/utils/unread.js
@@ -1,19 +1,20 @@
 import store from '@/store'
-import { getReleaseCount } from '@/api'
+import { getReleaseCount } from '@/api' // 之后用socket取代
 
 const unreadFun = () => {
   const nav = store.state.categoryItems
   const result = JSON.parse(JSON.stringify(nav))
-  console.log('-------->', result)
   result.children.forEach(item => {
     item.children.forEach(async (c) => {
       const res = await getReleaseCount({
         category: c.value
       })
-      console.log(c.text, '----->', res)
+      c.unread = +res.data || 0
+      store.commit('setState', {
+        categoryItems: result
+      })
     })
   })
-  return result
 }
 
 export default unreadFun
