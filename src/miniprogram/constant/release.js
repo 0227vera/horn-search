6dad43bc5a66c46c5865de386356783d10aad16f
salvatore <liaoxuan@salvatoredeMacBook-Pro.local>
2023-09-25 23:41:27 +0800

    perf: boss finish

--- src/miniprogram/constant/release.js
+++ src/miniprogram/constant/release.js
@@ -7,29 +7,29 @@ const category = [
       { text: "拉布", id: "1-6", showCategoryType: true },
       { text: "大烫", id: "1-9", showCategoryType: true },
       { text: "中烫", id: "1-12", showCategoryType: true },
-      { text: "剪线", id: "1-12", showCategoryType: true },
-      { text: "包装", id: "1-15", showCategoryType: true },
-      { text: "成件车位", id: "1-18", showCategoryType: true },
-      { text: "四线/五线", id: "1-21", showCategoryType: true },
-      { text: "平车", id: "1-24", showCategoryType: true },
-      { text: "冚车", id: "1-27", showCategoryType: true },
-      { text: "卷边", id: "1-30", showCategoryType: true },
-      { text: "做领上领", id: "1-33", showCategoryType: true },
-      { text: "上腰", id: "1-36", showCategoryType: true },
-      { text: "车门筒", id: "1-39", showCategoryType: true },
-      { text: "车口袋", id: "1-42", showCategoryType: true },
-      { text: "模板绗线", id: "1-45", showCategoryType: true },
-      { text: "塞棉/拍棉", id: "1-48", showCategoryType: true },
-      { text: "手工类", id: "1-51", showCategoryType: true, needDetail: true },
-      { text: "专机类", id: "1-54", showCategoryType: true, needDetail: true },
-      { text: "指导工/工艺", id: "1-57", categoryType: '2' },
-      { text: "收发", id: "1-60", showCategoryType: true, categoryType: '2' },
-      { text: "跟单", id: "1-63", showCategoryType: true, categoryType: '2' },
-      { text: "仓管", id: "1-66", showCategoryType: true, categoryType: '2' },
-      { text: "验布", id: "1-69", showCategoryType: true, categoryType: '2' },
-      { text: "做饭工", id: "1-72", showCategoryType: true, categoryType: '2' },
-      { text: "杂工", id: "1-75", showCategoryType: true, categoryType: '2' },
-      { text: "其他", id: "1-78", showCategoryType: true, categoryType: '2', needDetail: true }
+      { text: "剪线", id: "1-15", showCategoryType: true },
+      { text: "包装", id: "1-18", showCategoryType: true },
+      { text: "成件车位", id: "1-21", showCategoryType: true },
+      { text: "四线/五线", id: "1-24", showCategoryType: true },
+      { text: "平车", id: "1-27", showCategoryType: true },
+      { text: "冚车", id: "1-30", showCategoryType: true },
+      { text: "卷边", id: "1-33", showCategoryType: true },
+      { text: "做领上领", id: "1-36", showCategoryType: true },
+      { text: "上腰", id: "1-39", showCategoryType: true },
+      { text: "车门筒", id: "1-42", showCategoryType: true },
+      { text: "车口袋", id: "1-45", showCategoryType: true },
+      { text: "模板绗线", id: "1-48", showCategoryType: true },
+      { text: "塞棉/拍棉", id: "1-51", showCategoryType: true },
+      { text: "手工类", id: "1-54", showCategoryType: true, needDetail: true },
+      { text: "专机类", id: "1-57", showCategoryType: true, needDetail: true },
+      { text: "指导工/工艺", id: "1-60", categoryType: '2' },
+      { text: "收发", id: "1-63", showCategoryType: true, categoryType: '2' },
+      { text: "跟单", id: "1-66", showCategoryType: true, categoryType: '2' },
+      { text: "仓管", id: "1-69", showCategoryType: true, categoryType: '2' },
+      { text: "验布", id: "1-72", showCategoryType: true, categoryType: '2' },
+      { text: "做饭工", id: "1-75", showCategoryType: true, categoryType: '2' },
+      { text: "杂工", id: "1-78", showCategoryType: true, categoryType: '2' },
+      { text: "其他", id: "1-81", showCategoryType: true, categoryType: '2', needDetail: true }
     ]
   },
   {
@@ -81,4 +81,9 @@ export default {
     category,
     form: ['price', 'poi', 'tel', 'time', 'images', 'note']
   }
+}
+
+export const categoryMap = {
+  findWorker: 'findWorker',
+  findJob: 'findWorker'
 }
